bf24e748b57a1f971f6a7a8e1cb0b63f6a770435
Ericao327 <ericlopesdemedeiros@gmail.com>
2026-03-24 10:13:17 -0300

    Atualizando livros na biblioteca

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id: 1,
+        titulo: "O Senhor dos Anéis",
+    },
+    {
+        id: 2,
+        titulo: "O Hobbit",
+    }
+]
 
 app.get("/", (req, res) => {
   res.status(200).send("Curso de Node.Js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("livro adicionado com sucesso");
 });
 
 export default app;
